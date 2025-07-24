34c26437c8ef57c8b926657c5e44002eccacb8e0
AakanshaAghav <aakanshaaghav4@gmail.com>
2025-07-24 12:55:51 +0530

    Updated CSS styles for better layout

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 'use strict';
 
-const secretNumber = Math.trunc(Math.random()*20)+1;
+let secretNumber = Math.trunc(Math.random()*20)+1;
+
 let score = 20;
 let highscore = 0;
 
-document.querySelector('.number').textContent = secretNumber;
-document.querySelector('.check').addEventListener('click', function() {
+
+document.querySelector('.check').addEventListener('click', function(){
     const guess = Number(document.querySelector('.guess').value);
 
     console.log(guess, typeof guess);
@@ -16,8 +17,18 @@ document.querySelector('.check').addEventListener('click', function() {
 
     } else if (guess === secretNumber){ // When Player Wins!
         document.querySelector('.message').textContent = '🎉 Correct Number!';
-        highscore++;
+        
+        document.querySelector('.number').textContent = secretNumber;
+
+        if(score > highscore){
+            highscore = score;
+        }
+
         document.querySelector('.highscore').textContent = highscore;
+
+        document.querySelector('body').style.backgroundColor = "#60b347";
+
+        document.querySelector('.number').style.width = "30rem";
 
     } else if (guess > secretNumber){ // When guess is too high
         if (score > 1){
@@ -39,5 +50,19 @@ document.querySelector('.check').addEventListener('click', function() {
             document.querySelector('.score').textContent = 0;
         }
     }
-    
+});
+
+document.querySelector('.again').addEventListener('click',function(){
+      // Reset Values
+      score = 20;
+      secretNumber = Math.trunc(Math.random() * 20) + 1;
+
+      // Reset UI
+      document.querySelector('.message').textContent = 'Start guessing...';
+      document.querySelector('.score').textContent = score;
+      document.querySelector('.number').textContent = '?';
+      document.querySelector('.guess').value = '';
+
+      document.querySelector('body').style.backgroundColor = '#222';
+      document.querySelector('.number').style.width = '15rem';
 });
